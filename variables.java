790595afbda6dd0628fdf0c4f0073935db652849
Alexis <franklinfloresra@gmail.com>
2022-10-21 15:08:42 -0600

    commit -m "Version 2"

--- variables.java
+++ variables.java
@@ -1,3 +1,18 @@
 public class variables {
+    // @Link-jpg
+    public static void main(String[] args) {
+        // declaracion de Variables
+        String casa; // Variable de tipo texto o cadena
+        casa = "Casa de Color Azul";
+        System.out.println("Dato guardado en la variable casa" + casa);
+        int cuartos;
+        cuartos = 2;
+        double puerta;
+        puerta = 1.75;
+        System.out.println(casa + "con" + cuartos
+                + "habitaciones"
+                + " y una puerta de "
+                + puerta + "mts");
 
+    }
 }
